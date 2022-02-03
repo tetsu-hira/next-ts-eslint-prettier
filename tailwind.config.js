92e8a20821e1ea78c0055a09049f0904e40a7fdb
tetsu.hira <svc.snowdrop@gmail.com>
2022-02-03 09:51:34 +0900

    開発環境完成

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  mode: 'JIT',
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  purge: ['src/pages/**/*.{js,ts,jsx,tsx}', 'src/components/**/*.{js,ts,jsx,tsx}'],
+};
