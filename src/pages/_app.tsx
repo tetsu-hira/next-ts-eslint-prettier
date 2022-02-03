22f5d097ac100da05a625e57c5807295b4b6eb58
tetsu.hira <svc.snowdrop@gmail.com>
2022-02-03 18:05:00 +0900

    アニメーションを追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,9 +3,16 @@
 // import '../styles/globals.css'
 // 追記
 import 'tailwindcss/tailwind.css';
+import { AnimatePresence } from 'framer-motion';
 import type { AppProps } from 'next/app';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps, router }: AppProps) {
+  return (
+    <div>
+      <AnimatePresence exitBeforeEnter>
+        <Component {...pageProps} key={router.route} />;
+      </AnimatePresence>
+    </div>
+  );
 }
 export default MyApp;
